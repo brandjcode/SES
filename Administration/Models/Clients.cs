5b7f48163a2410298ddb9788794f0fa897107a85
joneseysinno <ajones21@gmail.com>
2023-06-05 16:16:05 -0600

    Add project files.

---
+++ Administration/Models/Clients.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administration.Models
+{
+    public class Clients
+    {
+        public string ClientName { get; set; }
+
+        //Must be unique
+        //3 to 5 letter abbreviation for each client
+        //[Unique]
+        public string ClientID { get; set; }
+
+        //grab from contacts list
+        public string[] ClientContacts { get; set; }
+
+    }
+}
